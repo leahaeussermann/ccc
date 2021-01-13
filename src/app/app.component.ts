6d7e3fb1b0a1f2f64993d4af6de4d01cf2210a59
haeul <lea.haeussermann@akka.eu>
2021-01-13 19:15:13 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'recipe-book';
   public isCollapsed = true;
+
+  refresh() {
+    window.location.reload();
+  }
 }
